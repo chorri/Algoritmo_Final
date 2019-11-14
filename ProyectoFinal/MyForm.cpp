5c7aca60956a4c27922aa3a22c4f293e40ca2fe4
Chorri <josemariacosta_8@hotmail.com>
2019-11-13 21:27:25 -0500

    Agregado lectura de archivo mejorada

--- ProyectoFinal/MyForm.cpp
+++ ProyectoFinal/MyForm.cpp
@@ -2,6 +2,9 @@
 #include "MyForm.h"
 #include <iostream>
 
+#include <Windows.h>
+#include <sstream>
+
 #include <filesystem>
 #include <string>
 
@@ -9,14 +12,40 @@ using namespace std;
 using namespace std::experimental::filesystem;
 using namespace ProyectoFinal;
 
+WIN32_FILE_ATTRIBUTE_DATA dat;
+
+
+void eraseSubStr(std::string & mainStr, const std::string & toErase)
+{
+	// Search for the substring in string
+	size_t pos = mainStr.find(toErase);
+	if (pos != std::string::npos)
+	{
+		// If found then erase it from string
+		mainStr.erase(pos, toErase.length());
+	}
+}
 
 
 int main() {
-
-	string path = "C:\\Users\\Proyecto\\Documents\\New folder\\Test";
-	for (const auto& entry : recursive_directory_iterator(path)) {
+	string path = "C:\\Users\\Juan\\Desktop\\UPC\\TercerCiclo\\Algoritmo\\TrabajoFinal\\Algoritmo_Final\\ProyectoFinal\\Debug\\ProyectoFinal.tlog";
+	for (const auto& entry : directory_iterator(path)) {
 		cout << "- ";
-		cout << entry.path() << std::endl;
+		//cout << entry.path() << std::endl;
+		
+		GetFileAttributesEx(entry.path().c_str(), GetFileExInfoStandard, &dat);
+		
+		std::stringstream sstr;
+		sstr << dat.nFileSizeLow;
+		std::string str = sstr.str();
+
+		//cout << endl << entry.path().filename() << endl;
+
+		//eraseSubStr(entry.path().string(), path);
+
+		
+
+		cout << entry.path().filename() << " | " << str << endl;
 	}
 
 	cout << "test" << endl;
